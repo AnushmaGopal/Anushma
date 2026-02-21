7d1fba18a0c7578b12f1cde3c4e4dd57ff64b492
amalk <amalk@AMAL-NA433>
2026-02-21 14:18:42 +0530

    kawasaki2 class added

--- SampleMaven_10/src/test/java/bikes/KawasakiTest2.java
+++ SampleMaven_10/src/test/java/bikes/KawasakiTest2.java
@@ -5,12 +5,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class KawasakiTest {
+public class KawasakiTest2 {
 	@Test
 	public void launch() {
 		WebDriver driver=new ChromeDriver();
 		
 		driver.get("https://www.kawasaki-india.com/");
-		Reporter.log("Kawasaki launched successfully",true);
+		Reporter.log("Kawasaki2 launched",true);
 	}
 }
